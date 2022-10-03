b4a262123cedb96d743e76431ad8956143d12c97
unknown <antudippolito123@gmail.com>
2022-10-03 14:56:56 -0300

    d

--- api/src/routes/TemperRequests.js
+++ api/src/routes/TemperRequests.js
@@ -8,27 +8,28 @@ let allTempers;
 axios.get('https://api.thedogapi.com/v1/breeds')
     .then(async data => {
 
-        allTempers = data.data.map(temper=>{
+        allTempers = data.data.map(temper => {
             return temper.temperament;
         }).join(',').replace(/\s+/g, '').split(',');
-       
+
         // if(!allTempers.inclides('')){
         //     Temper.bulkCreate(allTempers);
 
         // }
         console.log(allTempers);
         allTempers.forEach(async element => {
-            if(element!==''){
-                const el = await Temper.findOrCreate({where:{name:element}});
+            if (element !== '') {
+                const el = await Temper.findOrCreate({ where: { name: element } });
+                console.log(el);
             }
         });
     })
 
-    router.get('/',async (req,res,next)=>{
-        const Tempers= await Temper.findAll();
-        const temperNames = Tempers.map(e=>e.name);
-        res.json(temperNames);
-    });
+router.get('/', async (req, res, next) => {
+    const Tempers = await Temper.findAll();
+    const temperNames = Tempers.map(e => e.name);
+    res.json(temperNames);
+});
 
 
-module.exports= router;
+module.exports = router;
